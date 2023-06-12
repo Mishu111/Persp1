fca6a735c4d272ae716bb3ae03e5de4b850c64dd
Sdac <Sdac@Sdac-PC>
2023-06-12 11:18:49 +0530

    hgyufyut

--- submain/src/submain/neeww.java
+++ submain/src/submain/neeww.java
@@ -1,7 +1,8 @@
 package submain;
 
 public class neeww {
-public static void main(String[] args) {
-	System.out.println("submain neww branch");
-}
+	public static void main(String[] args) {
+		System.out.println("submain neww branch");
+		System.out.println("submain neww branch");
+	}
 }
